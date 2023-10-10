001be534716fa4782547b4f7aa63ce40e0ecf31c
JosephOdhiambo <odhisjoseph85@gmail.com>
2023-10-10 11:03:23 +0300

    Created module
    Generated module within backend of the yii2 advanced template. I was able to setup model, view and controller within the settings module. The crud generator kept adding the view into another directory but I made it work

---
+++ backend/modules/settings/views/companies/_search.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var backend\modules\settings\models\CompaniesSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="companies-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'company_id') ?>
+
+    <?= $form->field($model, 'company_name') ?>
+
+    <?= $form->field($model, 'company_email') ?>
+
+    <?= $form->field($model, 'company_address') ?>
+
+    <?= $form->field($model, 'company_created_date') ?>
+
+    <?php // echo $form->field($model, 'company_status') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/web/backend/modules/settings/companies/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use backend\modules\settings\models\Companies;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var backend\modules\settings\models\CompaniesSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Companies';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="companies-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Companies', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'company_id',
+            'company_name',
+            'company_email:email',
+            'company_address',
+            'company_created_date',
+            //'company_status',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Companies $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'company_id' => $model->company_id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
