1bb2017b26107a0de745a75973353d66a91e0381
JosephOdhiambo <odhisjoseph85@gmail.com>
2023-10-12 11:11:48 +0300

    Dependent Drop Down Lists
    Basically this section dealt with coding of dependent dropdown of branches list based on the company name selected.
    I added code to the following classes:
    
    public function actionLists($id)
        {
            $countBranches = Branches::find()
                ->where(['companies_company_id' => $id])
                ->count();
    
            $branches = Branches::find()
                ->where(['companies_company_id' => $id])
                ->all();
    
            if($countBranches > 0){
                foreach($branches as $branch){
                    echo "<option value='" . $branch->branch_id . "'>" . $branch->branch_name . "</option>";
    
                }
            }
        }
    
    I replaced the company_name dropdown in frontend\views\departments\_form.php and now it looks like this:
    
        <?= $form->field($model, 'companies_company_id')->dropDownList(
            ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
            [
                'prompt' => 'Select Company',
                'onchange' => <<<JS
                $.post("index.php?r=branches/lists&id=" + $(this).val(), function(data) {
                    console.log(data);
                    $("select#departments-branches_branch_id").html(data);
                });
            JS
            ]
        ) ?>
    
    I got the 'departments-branches_branch_id' by right clicking and inspecting the dropdown menu under branches name.

--- frontend/views/departments/_form.php
+++ frontend/views/departments/_form.php
@@ -17,17 +17,25 @@ use yii\jui\DatePicker;
     <?php $form = ActiveForm::begin(); ?>
    <?= $form->field($model, 'department_name')->textInput(['maxlength' => true]) ?>
 
-            <?= $form->field($model, 'branches_branch_id')->dropDownList(
-            ArrayHelper::map(Branches::find()->all(), 'branch_id','branch_name'),
-            ['prompt'=>'Select Branches']
-    ) 
-    ?>
+    <?= $form->field($model, 'companies_company_id')->dropDownList(
+        ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
+        [
+            'prompt' => 'Select Company',
+            'onchange' => <<<JS
+            $.post("index.php?r=branches/lists&id=" + $(this).val(), function(data) {
+                console.log(data);
+                $("select#departments-branches_branch_id").html(data);
+            });
+        JS
+        ]
+    ) ?>
 
-            <?= $form->field($model, 'companies_company_id')->dropDownList(
-            ArrayHelper::map(Companies::find()->all(), 'company_id','company_name'),
-            ['prompt'=>'Select Company']
-    ) 
-    ?>
+    <?= $form->field($model, 'branches_branch_id')->dropDownList(
+        ArrayHelper::map(Companies::find()->all(), 'branch_id', 'branch_name'),
+        [
+            'prompt' => 'Select Branch',
+        ]
+    ) ?>
 
     <?= $form->field($model, 'department_created_date')->widget(DatePicker::class, [
         'options' => [
