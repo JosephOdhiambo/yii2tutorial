05d8a3739bc957963eec379c102f96461dd9353e
JosephOdhiambo <odhisjoseph85@gmail.com>
2023-10-13 04:37:05 +0300

    Replace database file with new one

--- yii_advanced.sql
+++ yii_advanced.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2023 at 08:08 PM
+-- Generation Time: Oct 13, 2023 at 03:36 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -117,15 +117,11 @@ CREATE TABLE `branches` (
 --
 
 INSERT INTO `branches` (`branch_id`, `companies_company_id`, `branch_name`, `branch_address`, `branch_created_date`, `branch_status`) VALUES
-(1, 1, 'Main Branch', 'some branch address', '2023-10-07 02:10:48', 'active'),
-(2, 2, 'Town Branch', 'Nearby', '2023-10-07 02:10:15', 'active'),
-(3, 2, 'Wagithomo', 'Westside', '2023-10-07 03:10:50', 'inactive'),
-(4, 1, 'Main Branch', 'some branch address', '2023-10-08 00:00:00', 'active'),
-(5, 3, 'UON Branch', 'Mamlak', '2023-10-07 00:00:00', 'active'),
-(6, 3, 'UON Branch', 'Kumbaya', '2023-10-09 00:00:00', 'active'),
-(7, 3, 'Kitui', 'Far away', '2023-10-25 00:00:00', 'active'),
-(8, 2, 'Kanye West Branch', 'Around', '2024-01-17 00:00:00', 'active'),
-(9, 5, 'Wstside', 'Home', '2024-02-21 00:00:00', 'active');
+(10, 28, 'Main Branch', 'some branch address', '2023-10-16 00:00:00', 'active'),
+(11, 28, 'Town Branch', 'Westside', '2024-01-17 00:00:00', 'inactive'),
+(12, 29, 'Wstside', 'Around', '2023-10-15 00:00:00', 'inactive'),
+(13, 30, 'Kitui', 'Far away', '2023-10-31 00:00:00', 'active'),
+(14, 29, 'Absa Bank', 'Hfmail', '2023-10-31 00:00:00', 'active');
 
 -- --------------------------------------------------------
 
@@ -149,27 +145,23 @@ CREATE TABLE `companies` (
 --
 
 INSERT INTO `companies` (`company_id`, `company_name`, `company_email`, `company_address`, `logo`, `company_start_date`, `company_created_date`, `company_status`) VALUES
-(1, 'ABC', 'abc@gmail.com', 'some address', 'uploads/ABC.jpg', '2023-10-04', '2023-10-11', 'active'),
-(2, 'BCA', 'bca@hmail.com', 'very very far', '', '2024-05-15', '2023-10-07', 'inactive'),
-(3, 'No Cmpany', 'odhisjoseph85@gmail.com', 'Mamlaka', '', '2023-05-02', '2023-10-23', 'active'),
-(4, 'Jay Rock', 'rock@gmail.com', 'Block LLC', '', '2023-08-16', '2023-07-18', 'active'),
-(5, 'Ibtihaj', 'ib@email.com', 'tihaj LLC', '', '0000-00-00', '2023-10-24', 'active'),
-(6, 'Qing', 'madi@email.com', 'Nigeria', '', '0000-00-00', '2023-07-18', 'inactive'),
-(7, 'Waitha', 'ithaa@gmail.com', 'Laka', '', '0000-00-00', '2023-08-23', 'inactive'),
-(8, 'Side', 'dise@gmail.com', 'Milmani', '', '0000-00-00', '2023-12-20', 'active'),
-(9, 'walid', 'lid@rmail.com', 'booking', '', '0000-00-00', '2023-12-19', 'inactive'),
-(10, 'No Cmpany', 'odhisjoseph85@gmail.com', 'Mamlaka', '', '0000-00-00', '2023-07-20', 'active'),
-(11, 'Njaka', 'nj@fmail.com', 'shit ', '', '0000-00-00', '2023-05-23', 'active'),
-(12, 'No Cmpany', 'millions85@gmail.com', 'Buru', '', '0000-00-00', '2024-03-19', 'active'),
-(13, 'Kanye', 'west@gmail.com', 'Rocking', '', '2023-10-20', '2023-10-24', 'inactive'),
-(14, 'wallahi', 'thanks@email.com', 'nearby', '', '2023-10-12', '2023-10-18', 'inactive'),
-(15, 'kwanza', 'ghost@gmail.com', 'Mamlaka', '', '2023-10-18', '0000-00-00', 'active'),
-(21, 'No Cmpany2', 'odhisjoseph85@gmail.com', 'Mamlaka', '', '0000-00-00', '2023-10-20', 'active'),
-(22, 'No Cmpany2', 'odhisjoseph85@gmail.com', 'Mamlaka', '', '0000-00-00', '2023-10-31', 'active'),
-(23, 'No Cmpany2', 'odhisjoseph85@gmail.com', 'Mamlaka', '', '0000-00-00', '2023-10-31', 'active'),
-(24, 'wlakssk', 'W@email.com', 'lid', '', '0000-00-00', '2023-10-30', 'active'),
-(26, 'Lion King', 'king@gmail.com', 'Simba', 'uploads/Lion King.jpg', '0000-00-00', '2023-10-11', 'active'),
-(27, 'Niqqa prods', 'odhisjoseph85@gmail.com', 'Mamlaka', 'uploads/Niqqa prods.jpg', '0000-00-00', '2023-10-11', 'inactive');
+(28, 'ABC', 'doingIT@email.com', 'Far', 'uploads/ABC.png', '0000-00-00', '2023-10-12', 'active'),
+(29, 'DoingITEasy', 'doing@email.com', 'near', 'uploads/DoingITEasy.jpg', '0000-00-00', '2023-10-12', 'active'),
+(30, 'DoingITEasyChannel', 'channel@gmail.com', 'ksdjcsd', 'uploads/DoingITEasyChannel.jpg', '0000-00-00', '2023-10-12', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(100) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -186,14 +178,41 @@ CREATE TABLE `departments` (
   `department_status` enum('active','inactive') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `departments`
+-- Table structure for table `email`
 --
 
-INSERT INTO `departments` (`department_id`, `branches_branch_id`, `department_name`, `companies_company_id`, `department_created_date`, `department_status`) VALUES
-(1, 2, 'I.T Department', 1, '1997-12-13 00:00:00', 'active'),
-(2, 4, 'Eating Department', 2, '2023-10-09 00:00:00', 'active'),
-(3, 3, 'Business Department', 1, '0000-00-00 00:00:00', 'inactive');
+CREATE TABLE `email` (
+  `id` int(11) NOT NULL,
+  `receiver_name` varchar(50) NOT NULL,
+  `receiver_email` varchar(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `attachment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`location_id`, `zip_code`, `city`, `province`) VALUES
+(1, '1111', 'Colombo', 'Western'),
+(2, '2222', 'Galle', 'Southern');
 
 -- --------------------------------------------------------
 
@@ -289,12 +308,30 @@ ALTER TABLE `companies`
   ADD PRIMARY KEY (`company_id`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
 -- Indexes for table `departments`
 --
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`department_id`),
   ADD KEY `branches_branch_id` (`branches_branch_id`,`companies_company_id`),
   ADD KEY `companies_company_id` (`companies_company_id`);
+
+--
+-- Indexes for table `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location_id`);
 
 --
 -- Indexes for table `migration`
@@ -319,19 +356,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `branches`
 --
 ALTER TABLE `branches`
-  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
-  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
   MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `email`
+--
+ALTER TABLE `email`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
